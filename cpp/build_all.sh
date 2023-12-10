74fc1844e07f476ebc7d8f76ae337eb7681d333f
joe <joe@email.com>
2023-12-10 15:43:18 +0000

    build

--- cpp/build_all.sh
+++ cpp/build_all.sh
@@ -2,9 +2,9 @@
 
 TOP=$PWD
 
-for i in 01 02 03 04 05 07 08 09 10
+for i in 01 02 03 04 05 06 07 08 09 10
 do
     cd $TOP/$i
-    #g++ main.cpp -O3 -std=c++23
-    clang++ main.cpp -O3 -std=c++2b
+    g++ main.cpp -O3 -std=c++23 -o a.clang
+    clang++ main.cpp -O3 -std=c++2b -o a.gnu
 done
--- cpp/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TOP=$PWD
-
-for i in 01 02 03 04 05 07 08 09 10
-do
-  cd $TOP/$i
-  ./a.out
-done
